0c2a56b51fea30b4f642bc13414883f23fc4a4d8
carlosbenavides123 <carlos7legend@gmail.com>
2018-06-08 22:17:45 -0700

    fixed html/css for card for content

--- resources/views/content.blade.php
+++ resources/views/content.blade.php
@@ -7,93 +7,61 @@
 
                 <h3 class="text-center">Discussions</h3>
                 <p></p>
+
                 <div class="row">
+                    <div class="col-md-8 col-sm-12">
 
-                    <div class="col-md-8 col-sm-12 card card-body ">
-                        <div class="row" >
-                            <div style="display: inline-grid; margin: 0px 25px 0px 25px;">
-                                <span><img src="img/upvote.png" alt=""></span>
-                                Number
-                                <span><img src="img/downvote.png" alt=""></span>
+                            <div class="card card-body ">
+                                <div class="row" >
+
+                                    <div style="display: inline-grid; margin: 0px 25px 0px 25px;">
+                                        <span><img src="img/upvote.png" alt=""></span>
+                                        Number
+                                        <span><img src="img/downvote.png" alt=""></span>
+                                    </div>
+
+                                    <div>
+                                        <img src="img/test.jpg"style="height: 120px;width: 100px; display: inline-block;margin-top: 2.5%;">
+                                    </div>
+
+                                    <div style="display: inline-block; margin-left: 15px;">
+                                        <p style="position: absolute;">Lorem ipsum dolor sit amet, consecdadsqwrwr23q 324 42 ewrwe rwerrewrwerw rewr </p>
+                                    </div>
+
+                                </div>
                             </div>
 
-                            <div>
-                                <img src="img/test.jpg"style="height: 120px;width: 100px; display: inline-block;margin-top: 2.5%;">
+
+
+                            <div class="card card-body"style="margin-top: 2.5%">
+                                <div class="row" >
+
+                                    <div style="display: inline-grid; margin: 0px 25px 0px 25px;">
+                                        <span><img src="img/upvote.png" alt=""></span>
+                                        Number
+                                        <span><img src="img/downvote.png" alt=""></span>
+                                    </div>
+
+                                    <div>
+                                        <img src="img/test.jpg"style="height: 120px;width: 100px; display: inline-block;">
+                                    </div>
+
+                                    <div style="display: inline-block; margin-left: 15px;">
+                                        <p style="position: absolute;">Lorem ipsum dolor sit amet, consecdadsqwrwr23q 324 42 ewrwe rwerrewrwerw rewr </p>
+                                    </div>
+
+                                </div>
                             </div>
-
-                            <div style="display: inline-block; margin-left: 15px;">
-                                <p style="position: absolute;">Lorem ipsum dolor sit amet, consecdadsqwrwr23q 324 42 ewrwe rwerrewrwerw rewr </p>
-                            </div>
-                        </div>
-
-                        <br>
-
-
-                        <div class="row" style="min-width: 600px;">
-                            <div style="display: inline-grid; margin: 0px 25px 0px 25px;">
-                                <span><img src="img/upvote.png" alt=""></span>
-                                Number
-                                <span><img src="img/downvote.png" alt=""></span>
-                            </div>
-
-                            <div>
-                                <img src="img/test.jpg"style="height: 120px;width: 100px; display: inline-block;">
-                            </div>
-
-                            <div style="display: inline-block; margin-left: 15px;">
-                                <p style="position: absolute;">Lorem ipsum dolor sit amet, consecdadsqwrwr23q 324 42 ewrwe rwerrewrwerw rewr </p>
-                            </div>
-                        </div>
-
-                        <br>
-
-                        <div class="row" style="min-width: 600px;">
-                            <div style="display: inline-grid; margin: 0px 25px 0px 25px;">
-                                <span><img src="img/upvote.png" alt=""></span>
-                                Number
-                                <span><img src="img/downvote.png" alt=""></span>
-                            </div>
-
-                            <div>
-                                <img src="img/test.jpg"style="height: 120px;width: 100px; display: inline-block;">
-                            </div>
-
-                            <div style="display: inline-block; margin-left: 15px;">
-                                <p style="position: absolute;">Lorem ipsum dolor sit amet, consecdadsqwrwr23q 324 42 ewrwe rwerrewrwerw rewr </p>
-                            </div>
-                        </div>
-
-                        <br>
-
-                        <div class="row" style="min-width: 600px;">
-                            <div style="display: inline-grid; margin: 0px 25px 0px 25px;">
-                                <span><img src="img/upvote.png" alt=""></span>
-                                Number
-                                <span><img src="img/downvote.png" alt=""></span>
-                            </div>
-
-                            <div>
-                                <img src="img/test.jpg"style="height: 120px;width: 100px; display: inline-block;">
-                            </div>
-
-                            <div style="display: inline-block; margin-left: 15px;">
-                                <p style="position: absolute;">Lorem ipsum dolor sit amet, consecdadsqwrwr23q 324 42 ewrwe rwerrewrwerw rewr </p>
-                            </div>
-                        </div>
-
 
                     </div>
 
-                    <div class="col-md-4 col-sm-4">
+                    <div class="col-md-4">
+
                         Crappy Sidebar 2
                         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias aspernatur, dicta, dolores eaque eligendi error excepturi incidunt ipsa libero nostrum, obcaecati reiciendis suscipit ullam vel veritatis. Aspernatur at et fuga?</p>
                         <a href="/posts" class="btn btn-primary">Posts</a>
-                    </div>
-
 
                     </div>
 
-
-
-
+                </div>
     @endsection
